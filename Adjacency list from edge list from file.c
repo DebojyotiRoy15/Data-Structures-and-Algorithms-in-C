96d920369e9a745c31fb3321fba92a0ffddb5c1e
Debojyoti Roy <57871787+DebojyotiRoy15@users.noreply.github.com>
2020-02-11 17:02:06 +0530

    Update Adjacency list from edge list from file.c

--- Adjacency list from edge list from file.c
+++ Adjacency list from edge list from file.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+FILE *fp;
+
+struct node{
+  int v_weight;
+  int dest;
+  int deg;
+  struct node *next;
+};
+
+int main (void)
+{
+      struct node *new;
+      int vertices, source, dest, weight;
+      fp = fopen("adjacencylist.txt", "r");
+      if (fp!=NULL)
+      {
+                fscanf(fp, "%d ", &vertices);  
+                initialise (graph, vertices);
+                while(!feof(fp))
+                {
+                        fscanf(fp, "%d %d %d ", &source, &dest, &weight);
+                        new = (struct node *)malloc(sizeof(struct node));
+                        new->dest = dest;
+                        new->v_weight = weight;
+                        new->next = graph[source].next;
+                        graph[source].next = new;
+                        graph[source].deg++;
+                }
+      }     
+}
